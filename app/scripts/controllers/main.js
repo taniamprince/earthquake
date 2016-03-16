6e8262aad216481ac2a1091b4923ea10d8a3aaf2
taniamprince <taniamprince@gmail.com>
2016-03-16 14:04:19 +0000

    implemented largest earthquake stats

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,39 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name earthquakeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the earthquakeApp
- */
-angular.module('earthquakeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+var earthquakeApp = angular.module('earthquakeApp',[])
+
+earthquakeApp.controller('largest', ['$scope','$http', function($scope, $http) {
+	// Calculate start and end dates
+  	var date = new Date()
+
+  	if ($scope.param == "today") {
+	    var start = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + (date.getDate() - 1)
+		var end = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+	} else if ($scope.param == "week") {
+		var start = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + (date.getDate() - 7)
+		var end = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()	    
+	} else if ($scope.param == "month") {
+		var start = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
+		var end = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()	    
+	} else {
+	    var start = (date.getFullYear() - 1) + "-" + date.getMonth() + "-" + date.getDate()
+		var end = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+	}
+
+  	// Construct query url
+  	var url = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=magnitude&limit=1&starttime=" + start + "&endtime=" + end
+
+  	// Get JSON response
+  	$http.get(url)
+	.success(function(data, status, headers, config) {
+		var result = data.features.map(function (feature) {
+  			return feature.properties.title
+		});
+	    $scope.results = result.toString()
+	})
+	.error(function(error, status, headers, config) {
+		 $scope.results = "No data"
+	     console.log(status)
+	     console.log("Error occured")
+	});
+}]);
